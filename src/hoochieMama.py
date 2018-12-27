4cc488a0b0ba0a4135c40e4ff2d3d4e864d2c674
slawomir <slawomir.karol.domagala@gmail.com>
2018-12-27 15:09:25 +0100

    Smarter building revQueue using subDecks limits

--- src/hoochieMama.py
+++ src/hoochieMama.py
@@ -15,13 +15,13 @@ SORT_BY_OVERDUES="order by due"
 
 # == User Config =========================================
 
-#Prevents round-robin scheduling of forgotten cards.
-PRIORITIZE_TODAY = False
+# Prevents round-robin scheduling of forgotten cards.
+PRIORITIZE_TODAY = True
 
-#Randomize reviews per subdeck, makes custom_sort randomized by chunks.
-IMPOSE_SUBDECK_LIMIT = False
+# Randomize reviews per subdeck, makes custom_sort randomized by chunks.
+IMPOSE_SUBDECK_LIMIT = True
 
-#Default: Reviews are sorted by dues and randomized in chunks.
+# Default: Reviews are sorted by dues and randomized in chunks.
 CUSTOM_SORT = None
 # CUSTOM_SORT = SHOW_YOUNG_FIRST
 # CUSTOM_SORT = SHOW_MATURE_FIRST
@@ -31,36 +31,28 @@ CUSTOM_SORT = None
 
 # == End Config ==========================================
 
-## Performance Config ####################################
-
-# Performance impact cost O(n)
-# Uses quick shuffle if limit is exceeded.
-DECK_LIST_SHUFFLE_LIMIT = 128
-
-##########################################################
-
-
 import random
 import anki.sched
 from aqt import mw
 from anki.utils import ids2str
 from aqt.utils import showText
 from anki.hooks import wrap
+from anki.sched import Scheduler
 
 from anki import version
 ANKI21 = version.startswith("2.1.")
-on_sync=False
+on_sync = False
 
 
-#Turn this on if you are having problems.
+# Turn this on if you are having problems.
 def debugInfo(msg):
     # print(msg) #console
     # showText(msg) #Windows
     return
 
 
-#From: anki.schedv2.py
-#Mod:  Various, see logs
+# From: anki.schedv2.py
+# Mod:  Various, see logs
 def fillRev(self, _old):
     if self._revQueue:
         return True
@@ -70,29 +62,27 @@ def fillRev(self, _old):
 
     # This seem like old comments left behind, and does not affect current versions.
     # Remove these lines for testing
-    if self.col.decks.get(self.col.decks.selected(),False)['dyn']:
+    if self.col.decks.get(self.col.decks.selected(), False)['dyn']:
         # dynamic decks need due order preserved
         return _old(self)
-
 
     qc = self.col.conf
     if not qc.get("hoochieMama", False):
         return _old(self)
     debugInfo('using hoochieMama')
 
-
-    lim=currentRevLimit(self)
+    lim = Scheduler._currentRevLimit(self)
     if lim:
-        lim=min(self.queueLimit,lim)
-        sortBy=CUSTOM_SORT if CUSTOM_SORT else 'order by due'
+        lim = min(self.queueLimit, lim)
+        sort_by = CUSTOM_SORT if CUSTOM_SORT else 'order by due'
         if IMPOSE_SUBDECK_LIMIT:
-            self._revQueue=getRevQueuePerSubDeck(self,sortBy,lim)
+            self._revQueue = getRevQueuePerSubDeck(self, sort_by, lim)
         else:
-            self._revQueue=getRevQueue(self,sortBy,lim)
+            self._revQueue = getRevQueue(self, sort_by, lim)
 
         if self._revQueue:
             if CUSTOM_SORT and not IMPOSE_SUBDECK_LIMIT:
-                self._revQueue.reverse() #preserve order
+                self._revQueue.reverse()  # preserve order
             else:
                 # fixme: as soon as a card is answered, this is no longer consistent
                 r = random.Random()
@@ -107,164 +97,115 @@ def fillRev(self, _old):
         return self._fillRev()
 
 
-
-# In the world of blackjack, “penetration”, or “deck penetration”, 
+# In the world of blackjack, “penetration”, or “deck penetration”,
 # is the amount of cards that the dealer cuts off, relative to the cards dealt out.
 def getRevQueue(self, sortBy, penetration):
     debugInfo('v2 queue builder')
-    deckList=ids2str(self.col.decks.active())
-    revQueue=[]
+    dev_list = ids2str(self.col.decks.active())
+    rev_queue = []
 
     if PRIORITIZE_TODAY:
-        revQueue = self.col.db.list("""
+        dueToToday = self.col.db.list("""
 select id from cards where
 did in %s and queue = 2 and due = ?
-%s limit ?""" % (deckList, sortBy),
+%s limit ?""" % (dev_list, sortBy),
                 self.today, penetration)
+        dueToRest = self.col.db.list("""
+select id from cards where
+did in %s and queue = 2 and due < ?
+%s limit ?""" % (dev_list, sortBy),
+                self.today, penetration - len(dueToToday))
+        rev_queue.extend(dueToToday)
+        rev_queue.extend(dueToRest)
 
-    if not revQueue:
-        revQueue = self.col.db.list("""
+    else:
+        rev_queue = self.col.db.list("""
 select id from cards where
 did in %s and queue = 2 and due <= ?
-%s limit ?""" % (deckList, sortBy),
+%s limit ?""" % (dev_list, sortBy),
                 self.today, penetration)
 
-    return revQueue #Order needs tobe reversed for custom sorts
+    return rev_queue  # Order needs tobe reversed for custom sorts
 
 
-
-def getRevQueuePerSubDeck(sched,sortBy,penetration):
+def getRevQueuePerSubDeck(self,sortBy,penetration):
     debugInfo('per subdeck queue builder')
-    revQueue=[]
-    LEN=len(sched._revDids)
-    if LEN>10: #auto shuffles >=10, 50/5
-        if LEN>DECK_LIST_SHUFFLE_LIMIT: #segments
-            sched._revDids=cutDecks(sched._revDids,4) #0based
-        else: #shuffle deck ids
-            r=random.Random()
-            r.shuffle(sched._revDids)
+    deck_list = ids2str(self.col.decks.active())
+    rev_queue = []
 
-    pen=max(5,penetration//LEN) #if div by large val
-    for did in sched._revDids:
-        d=sched.col.decks.get(did)
-        lim=deckRevLimitSingle(sched,d,pen)
-        if not lim: continue
+    if PRIORITIZE_TODAY:
+        dueToToday = self.col.db.list("""
+    select id, did from cards where
+    did in %s and queue = 2 and due = ?
+    %s limit ?""" % (deck_list, sortBy),
+                                      self.today, penetration)
+        dueToRest = self.col.db.list("""
+    select id, did from cards where
+    did in %s and queue = 2 and due < ?
+    %s limit ?""" % (deck_list, sortBy),
+                                     self.today, penetration - len(dueToToday))
+        rev_queue.extend(dueToToday)
+        rev_queue.extend(dueToRest)
 
-        arr=None
-        if PRIORITIZE_TODAY:
-            arr=sched.col.db.list("""
-select id from cards where
-did = ? and queue = 2 and due = ?
-%s limit ?"""%sortBy, did, sched.today, lim)
+    else:
+        rev_queue = self.col.db.list("""
+    select id, did from cards where
+    did in %s and queue = 2 and due <= ?
+    %s limit ?""" % (deck_list, sortBy),
+                                    self.today, penetration)
 
-        if not arr:
-            arr=sched.col.db.list("""
-select id from cards where
-did = ? and queue = 2 and due <= ?
-%s limit ?"""%sortBy, did, sched.today, lim)
+    limited_rev_queue = []
+    decks = {deck['id']: deck for deck in self.col.decks.all()}
+    decks_used_limits = {did: 0 for did in decks.keys()}
+    decks_limits = {did: _deckRevLimit(self, d) for did, d in decks.items()}
+    for item in rev_queue:
+        did = item[1]
+        if decks_limits[did] <= decks_used_limits[did]:
+            continue
 
-        revQueue.extend(arr) 
-        if len(revQueue)>=penetration: break
-    return revQueue #randomized later
+        over_limit = False
+        for parent in self.col.decks.parents(did):
+            if decks_limits[parent['id']] <= decks_used_limits[parent['id']]:
+                over_limit = True
+                break
+        if over_limit:
+            continue
 
+        limited_rev_queue.append(item[0])
+        decks_used_limits[did] += 1
+        for parent in self.col.decks.parents(did):
+            decks_used_limits[parent['id']] += 1
 
-#Like cutting cards, this is a quick and dirty way to randomize the deck ids
-def cutDecks(queue,cnt=0):
-    total=len(queue)
-    p=random.randint(30,70) # %
-    cut=total*p//100
-    if cnt:
-        q=cutDecks(queue[cut:],cnt-1)
-        return q+cutDecks(queue[:cut],cnt-1)
-    return queue[cut:]+queue[:cut]
-
-
-
-#From: anki.schedv2.py
-def currentRevLimit(self):
-    d = self.col.decks.get(self.col.decks.selected(), default=False)
-    return deckRevLimitSingle(self, d)
+    return limited_rev_queue
 
 
-#From: anki.schedv2.py
-def deckRevLimitSingle(self, d, parentLimit=None):
+def _deckRevLimit(self, d):
     if not d: return 0  # invalid deck selected?
     if d['dyn']: return 99999
 
     c = self.col.decks.confForDid(d['id'])
-    lim = max(0, c['rev']['perDay'] - d['revToday'][1])
-    if parentLimit is not None:
-        return min(parentLimit, lim)
-    elif '::' not in d['name']:
-        return lim
-    else:
-        for parent in self.col.decks.parents(d['id']):
-            # pass in dummy parentLimit so we don't do parent lookup again
-            lim = min(lim, deckRevLimitSingle(self, parent, parentLimit=lim))
-        return lim
-
-
-
-
-#For reviewer count display (cosmetic)
-def resetRevCount(self, _old):
-    if on_sync:
-        return _old(self)
-
-    qc = self.col.conf
-    if not qc.get("hoochieMama", False):
-        return _old(self)
-
-    if IMPOSE_SUBDECK_LIMIT:
-        return _resetRevCountV1(self)
-    return _resetRevCountV2(self)
-
-
-#From: anki.schedv2.py
-def _resetRevCountV2(self):
-    lim = currentRevLimit(self)
-    self.revCount = self.col.db.scalar("""
-select count() from (select id from cards where
-did in %s and queue = 2 and due <= ? limit %d)""" % (
-        ids2str(self.col.decks.active()), lim), self.today)
-
-#From: anki.sched.py
-def _resetRevCountV1(self):
-    def _deckRevLimitSingle(d):
-        if not d: return 0
-        if d['dyn']: return 1000
-        c = mw.col.decks.confForDid(d['id'])
-        return max(0, c['rev']['perDay'] - d['revToday'][1])
-    def cntFn(did, lim):
-        return self.col.db.scalar("""
-select count() from (select id from cards where
-did = ? and queue = 2 and due <= ? limit %d)""" % lim,
-                                      did, self.today)
-    self.revCount = self._walkingCount(
-        _deckRevLimitSingle, cntFn)
-
+    return max(0, c['rev']['perDay'] - d['revToday'][1])
 
 
 anki.sched.Scheduler._fillRev = wrap(anki.sched.Scheduler._fillRev, fillRev, 'around')
-anki.sched.Scheduler._resetRevCount = wrap(anki.sched.Scheduler._resetRevCount, resetRevCount, 'around')
 if ANKI21:
     import anki.schedv2
     anki.schedv2.Scheduler._fillRev = wrap(anki.schedv2.Scheduler._fillRev, fillRev, 'around')
-    anki.schedv2.Scheduler._resetRevCount = wrap(anki.schedv2.Scheduler._resetRevCount, resetRevCount, 'around')
 
 
+# This monitor sync start/stops
+oldSync = anki.sync.Syncer.sync
 
-#This monitor sync start/stops
-oldSync=anki.sync.Syncer.sync
+
 def onSync(self):
     global on_sync
-    on_sync=True
-    ret=oldSync(self)
-    on_sync=False
+    on_sync = True
+    ret = oldSync(self)
+    on_sync = False
     return ret
 
-anki.sync.Syncer.sync=onSync
 
+anki.sync.Syncer.sync = onSync
 
 
 ##################################################
@@ -290,21 +231,26 @@ def setupUi(self, Preferences):
     self.hoochieMama.toggled.connect(lambda:toggle(self))
     self.gridLayout_4.addWidget(self.hoochieMama, r, 0, 1, 3)
 
+
 def __init__(self, mw):
     qc = self.mw.col.conf
-    cb=qc.get("hoochieMama", 0)
+    cb = qc.get("hoochieMama", 0)
     self.form.hoochieMama.setCheckState(cb)
+
 
 def accept(self):
     qc = self.mw.col.conf
     qc['hoochieMama']=self.form.hoochieMama.checkState()
+
 
 def toggle(self):
     checked=not self.hoochieMama.checkState()==0
     if checked:
         try:
             self.serenityNow.setCheckState(0)
-        except: pass
+        except:
+            pass
+
 
 aqt.forms.preferences.Ui_Preferences.setupUi = wrap(aqt.forms.preferences.Ui_Preferences.setupUi, setupUi, "after")
 aqt.preferences.Preferences.__init__ = wrap(aqt.preferences.Preferences.__init__, __init__, "after")
